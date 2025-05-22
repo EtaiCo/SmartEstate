1a55d6f475e9bc5d4cb5a7ea7f2c1503e62917b1
EtaiCohen <etaico@ac.sce.ac.il>
2025-05-22 21:00:06 +0300

    BSPM25T21-245 Typography

--- SmartEstate/src/Components/User/AdDetails.jsx
+++ SmartEstate/src/Components/User/AdDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import "./AdDetails.css";
 import { Container, Row, Col, Card, Spinner } from "react-bootstrap";
 import {
   FaBed,
