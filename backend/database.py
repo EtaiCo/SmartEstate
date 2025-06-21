e370ed457bbaea58022905a345aa3a52fa27deba
Hliran207 <hliran2@gmail.com>
2025-06-22 00:25:11 +0300

    pipeline

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,17 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-URL_DATABASE = 'postgresql+psycopg2://postgres:Hliran207!@localhost:5432/SmartEstate'
-engine = create_engine(URL_DATABASE)
+# Only switches to SQLite if this env variable is set
+USE_SQLITE = os.getenv("USE_SQLITE_FOR_TESTS") == "1"
 
-SessionLocal = sessionmaker(autocommit = False, autoflush= False, bind = engine)
+if USE_SQLITE:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+    connect_args = {"check_same_thread": False}
+else:
+    SQLALCHEMY_DATABASE_URL = "postgresql://localhost/YOUR_DB_NAME"
+    connect_args = {}
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-
